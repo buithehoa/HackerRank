366e54ed21857b9ae23629a5e7dcfa25ca9dbb98
Bui The Hoa <buithehoa@gmail.com>
2017-11-21 07:23:07 +0900

    Day 8: Dictionaries and Maps * Refactor the code into methods

--- 30DaysOfCode/day_08_dictionaries_and_map.py
+++ 30DaysOfCode/day_08_dictionaries_and_map.py
@@ -2,24 +2,35 @@
 
 import sys
 
+def read_entries():
+    entries = {}
+    for i in range(0, entry_count):
+        entry = input().strip().split(' ')
+        entries[entry[0]] = entry[1]
+
+    return entries
+
+def read_names():
+    names = []
+    name = input()
+    while name:
+        names.append(name)
+        try:
+            name = input()
+        except EOFError:
+            break
+
+    return names
+
+def print_entries(names):
+    for i in range(0, len(names)):
+        if names[i] in entries:
+            print("%s=%s" % (names[i], entries[names[i]]))
+        else:
+            print("Not found")
+
 entry_count = int(input())
+entries = read_entries()
+names = read_names()
 
-entries = {}
-for i in range(0, entry_count):
-    entry = input().strip().split(' ')
-    entries[entry[0]] = entry[1]
-
-names = []
-name = input()
-while name:
-    names.append(name)
-    try:
-        name = input()
-    except EOFError:
-        break
-
-for i in range(0, len(names)):
-    if names[i] in entries:
-        print("%s=%s" % (names[i], entries[names[i]]))
-    else:
-        print("Not found")
+print_entries(names)
