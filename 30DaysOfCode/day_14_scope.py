56feddd038180f5ffbd01df456463435d357972a
Bui The Hoa <buithehoa@gmail.com>
2017-12-02 00:29:16 +0700

    Day 14: Scope

---
+++ 30DaysOfCode/day_14_scope.py
@@ -0,0 +1,20 @@
+class Difference:
+    def __init__(self, elements):
+        self.__elements = elements
+        self.maximumDifference = 0
+
+    def computeDifference(self):
+        self.__elements.sort()
+
+        self.maximumDifference = self.__elements[len(self.__elements) - 1] - self.__elements[0]
+
+# End of Difference class
+
+if __name__ == "__main__":
+    _ = input()
+    a = [int(e) for e in input().split(' ')]
+
+    d = Difference(a)
+    d.computeDifference()
+
+    print(d.maximumDifference)
