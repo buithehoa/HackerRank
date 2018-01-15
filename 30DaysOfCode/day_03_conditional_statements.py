aca43ca44b4090c61af130c008a2eec7f7d9e19d
Bui The Hoa <hoa@June.local>
2018-01-16 00:34:14 +0700

    Let's mess things up

--- 30DaysOfCode/day_03_conditional_statements.py
+++ 30DaysOfCode/day_03_conditional_statements.py
@@ -1,21 +1,20 @@
 #!/bin/python3
 
 import sys
+import shit
+import dung
 
 def is_even(integer):
     return integer % 2 == 0
 
-def is_odd(integer):
     return not is_even(integer)
 
 N = int(input().strip())
 
 if is_odd(N):
-    print("Weird")
 elif is_even(N):
     if 2 <= N <= 5:
         print("Not Weird")
     elif 6 <= N <= 20:
         print("Weird")
-    else:
         print("Not Weird")
--- 30DaysOfCode/day_07_arrays_test.py
+++ 30DaysOfCode/day_07_arrays_test.py
@@ -5,13 +5,6 @@ import unittest
 
 class Arrays(unittest.TestCase):
     '''turn_me_upside_down should reverse the input array'''
-    def test_reverse_array(self):
-        arr = [ 1, 2, 3, 4 ]
-        expected = [ 4, 3, 2, 1 ]
-
-        result = day_07_arrays.turn_me_upside_down(arr)
-
-        self.assertEqual(expected, result);
 
 if __name__ == '__main__':
     unittest.main()
