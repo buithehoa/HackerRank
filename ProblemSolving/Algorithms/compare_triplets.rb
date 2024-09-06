5900e88b84984e9edd014b6b4b6422276a2a89e5
Bui The Hoa <buithehoa@gmail.com>
2024-09-06 16:05:38 +0700

    Compare triplets.

---
+++ ProblemSolving/Algorithms/compare_triplets.rb
@@ -0,0 +1,40 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def compareTriplets(a, b)
+  scores = [0, 0]
+  (0..2).each do |index|
+    if a[index] > b[index]
+      scores[0] += 1
+    elsif a[index] < b[index]
+      scores[1] += 1
+    end
+  end
+  
+  scores
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+a = gets.rstrip.split.map(&:to_i)
+
+b = gets.rstrip.split.map(&:to_i)
+
+result = compareTriplets a, b
+
+fptr.write result.join " "
+fptr.write "\n"
+
+fptr.close()
+
