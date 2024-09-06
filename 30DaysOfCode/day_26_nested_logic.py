c6008b13f7f6cef8953388448af6938cb35a2143
Bui The Hoa <buithehoa@gmail.com>
2024-09-06 15:49:42 +0700

    Add Day 26.

---
+++ 30DaysOfCode/day_26_nested_logic.py
@@ -0,0 +1,30 @@
+# Day 26: Nested Logic
+# https://www.hackerrank.com/challenges/30-nested-logic/problem
+
+import sys
+import math
+
+class PrimeNumber(object):
+    @classmethod
+    def is_prime(cls, n):
+        if n == 1: return False
+
+        sqrt = round(math.sqrt(n))
+        for i in range(1, sqrt + 1):
+            remainder = n % i
+            if remainder == 0 and i != 1:
+                return False
+
+        return True
+
+if __name__ == "__main__":
+    t = int(input())
+    n = []
+    for i in range(t):
+        n.append(int(input()))
+
+    for i in range(t):
+        if PrimeNumber.is_prime(n[i]):
+            print('Prime')
+        else:
+            print('Not prime')
